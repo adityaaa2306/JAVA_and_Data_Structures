f77f01edea445dd238313ad4f44b52d161fce524
adityaaa2306 <110329614+adityaaa2306@users.noreply.github.com>
2024-10-20 00:38:30 +0530

    Day 5: Practicing more Functions

---
+++ Functions_PracticeQs/Function6.java
@@ -0,0 +1,15 @@
+// Write an infinite loop using do while condition.
+
+package Functions_PracticeQs;
+
+public class Function6 {
+    public static void infiniteLoop() {
+        do {
+            System.out.println("This is an infinite loop. Press Ctrl+C to exit.");
+        } while (true);
+    }
+
+    public static void main(String[] args) {
+        infiniteLoop(); // Uncomment to run the infinite loop
+    }
+}
