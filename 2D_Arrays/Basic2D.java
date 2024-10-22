a9c69420c95634595694901f6d5a8c730b07c1c6
adityaaa2306 <110329614+adityaaa2306@users.noreply.github.com>
2024-10-22 23:58:27 +0530

    Add files via upload

---
+++ 2D_Arrays/Basic2D.java
@@ -0,0 +1,34 @@
+// Declaration
+
+// type[][] arrayName = new type [rows] [columns];
+// int[][] numbers = new int[3][5];
+
+package Arrays_2D;
+
+import java.util.Scanner;
+
+public class Basic2D {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rows = sc.nextInt();
+        int cols = sc.nextInt();
+
+        int[][] numbers = new int[rows][cols];
+
+        // input rows
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                numbers[i][j] = sc.nextInt();
+            }
+        }
+
+        // output
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(numbers[i][j] + " ");
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
