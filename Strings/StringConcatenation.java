1414ea309c8d31e81e71619b60bd16afa999e6d7
adityaaa2306 <110329614+adityaaa2306@users.noreply.github.com>
2024-10-23 00:00:00 +0530

    Add files via upload

---
+++ Strings/StringConcatenation.java
@@ -0,0 +1,12 @@
+package Arrays_2D;
+
+public class StringConcatenation {
+    public static void main(String[] args) {
+        // concatenation
+
+        String firstName = "tony";
+        String lastName = "stark";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+    }
+}
