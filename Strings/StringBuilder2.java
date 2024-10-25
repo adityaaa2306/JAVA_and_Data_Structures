2dc15ff46f3780599af6d8adac26e158bf7f29c7
adityaaa2306 <110329614+adityaaa2306@users.noreply.github.com>
2024-10-25 23:48:26 +0530

    Day 11: Practicing Strings Further

---
+++ Strings/StringBuilder2.java
@@ -0,0 +1,12 @@
+package Arrays_2D;
+
+public class StringBuilder2 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("h");
+        sb.append("e");
+        sb.append("l");
+        sb.append("l");
+        sb.append("o");
+        System.out.println(sb);
+    }
+}
