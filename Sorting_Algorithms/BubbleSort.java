44aaaf46f20dc72f5bff45b0268a139dcf71f474
adityaaa2306 <110329614+adityaaa2306@users.noreply.github.com>
2024-11-06 22:43:56 +0530

    Day 22: Learning Basic Sorting Algorithms

---
+++ Sorting_Algorithms/BubbleSort.java
@@ -0,0 +1,41 @@
+package Sorting_Algorithms;
+
+public class BubbleSort {
+    public static void bubbleSort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // Swap arr[j] and arr[j+1]
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    swapped = true;
+                }
+            }
+            // If no two elements were swapped by inner loop, array is sorted
+            if (!swapped) {
+                break;
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] data = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Unsorted array:");
+        printArray(data);
+
+        bubbleSort(data);
+        System.out.println("Sorted array:");
+        printArray(data);
+    }
+}
